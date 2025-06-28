65aecb8540a2138b48fa49a80b0e6b0524a5bdd3
laserda <komanatsefabriceigor@gmail.com>
2025-06-28 18:23:31 -0400

    Ajout de Entity sur UsersBoutique

--- backend/depensio.Domain/Models/UsersBoutique.cs
+++ backend/depensio.Domain/Models/UsersBoutique.cs
@@ -1,14 +1,10 @@
 ﻿namespace depensio.Domain.Models; 
 
-public class UsersBoutique
+public class UsersBoutique : Entity<Guid>
 {
     public string UserId { get; set; }
     public Guid BoutiqueId { get; set; }
     //public string Role { get; set; }
-    public DateTime CreatedAt { get; set; }
-    public DateTime UpdatedAt { get; set; }
-    public string CreatedBy { get; set; }
-    public string UpdatedBy { get; set; }
 
     //public ApplicationUser ApplicationUser { get; set; }
     public Boutique Boutique { get; set; }
