7649663091f87ab590c93d45ea66b8636135795f
laserda <komanatsefabriceigor@gmail.com>
2025-07-08 21:43:32 -0400

    Ajout de layout boutique

---
+++ frontend/depensio.Shared/Pages/FlowbitePage.cs
@@ -0,0 +1,19 @@
+﻿using depensio.Shared.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace depensio.Shared.Pages;
+
+public abstract class FlowbitePage : ComponentBase
+{
+    [Inject]
+    protected IFlowbiteService FlowbiteService { get; set; } = default!;
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            await FlowbiteService.InitializeFlowbiteAsync();
+        }
+        await base.OnAfterRenderAsync(firstRender);
+    }
+}
