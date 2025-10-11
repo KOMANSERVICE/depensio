41f3c0099593d71ab48dd6ed792331834a8d6cdd
laserda <komanatsefabriceigor@gmail.com>
2025-10-10 22:25:55 -0400

    fix

--- backend/depensio.Application/UseCases/Purchases/Commands/CreatePurchase/CreatePurchaseCommand.cs
+++ backend/depensio.Application/UseCases/Purchases/Commands/CreatePurchase/CreatePurchaseCommand.cs
@@ -11,14 +11,24 @@ public class CreatePurchaseCommandValidator : AbstractValidator<CreatePurchaseCo
 {
     public CreatePurchaseCommandValidator()
     {
-        RuleFor(x => x.Purchase).NotNull().NotEmpty().WithMessage("Purchase is required");
-        RuleFor(x => x.Purchase.Title).NotNull().NotEmpty().WithMessage("Title is required");
+        RuleFor(x => x.Purchase)
+            .NotNull().NotEmpty().WithMessage("L'achat est obligatoire.");
+        
+        RuleFor(x => x.Purchase.Title)
+            .NotNull().NotEmpty().WithMessage("Le titre est obligatoire.");
+        
         RuleFor(x => x.Purchase.DateAchat)
-        .NotNull().WithMessage("La date d'achat est requise")
-        .Must(date => date <= DateOnly.FromDateTime(DateTime.UtcNow))
-        .WithMessage("La date d'achat ne peut pas être postérieure à la date du jour.");
-        RuleFor(x => x.Purchase.SupplierName).NotNull().NotEmpty().WithMessage("SupplierName is required");
-        RuleFor(x => x.Purchase.Items).NotEmpty().WithMessage("Items is required");
-        RuleFor(x => x.Purchase.BoutiqueId).NotEmpty().WithMessage("BoutiqueId is required");
+            .NotNull().WithMessage("La date d'achat est obligatoire.")
+            .Must(date => date <= DateOnly.FromDateTime(DateTime.UtcNow))
+            .WithMessage("La date d'achat ne peut pas être postérieure à la date du jour.");
+        
+        RuleFor(x => x.Purchase.SupplierName)
+            .NotNull().NotEmpty().WithMessage("Le nom du fournisseur est obligatoire.");
+        
+        RuleFor(x => x.Purchase.Items)
+            .NotEmpty().WithMessage("Les articles sont obligatoires.");
+        
+        RuleFor(x => x.Purchase.BoutiqueId)
+            .NotEmpty().WithMessage("L'identifiant de la boutique est obligatoire.");
     }
 }
