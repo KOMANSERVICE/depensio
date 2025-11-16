db825c6e05ef73d33d2d85d0b7a30c6b7b27756f
laserda <komanatsefabriceigor@gmail.com>
2025-11-16 15:12:31 -0500

    Reccuperation des menus depuis le service menu

--- backend/depensio.Application/Services/MenuService.cs
+++ backend/depensio.Application/Services/MenuService.cs
@@ -1,4 +1,5 @@
-﻿using depensio.Application.Interfaces;
+﻿using depensio.Application.ApiExterne.Menus;
+using depensio.Application.Interfaces;
 using depensio.Application.UseCases.Menus.DTOs;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -7,56 +8,65 @@ using System.Threading;
 namespace depensio.Application.Services;
 
 public class MenuService(
-        IDepensioDbContext _dbContext
+        IDepensioDbContext _dbContext,
+        IMenuService menuServiceApi
     ) 
 
 {
 
     public async Task<List<MenuUserDTO>> GetMenuByUserBoutiqueAsync(string userId, Guid boutiqueId)
     {
-        var isOwner = await _dbContext.Boutiques
-            .AnyAsync(b => b.Id == BoutiqueId.Of(boutiqueId)
-            && b.OwnerId == userId
-            && b.UsersBoutiques.Any(ub => ub.UserId == userId));
+        //var isOwner = await _dbContext.Boutiques
+        //    .AnyAsync(b => b.Id == BoutiqueId.Of(boutiqueId)
+        //    && b.OwnerId == userId
+        //    && b.UsersBoutiques.Any(ub => ub.UserId == userId));
 
-        List<MenuUserDTO> menus;
+        //List<MenuUserDTO> menus;
 
-        if (isOwner)
+        //if (isOwner)
+        //{
+        //    //TODO: (A revoir, reccuperer les menus du plan) deja correct
+        //    // juste mettre un filtre sur chaque API pour s'assurer que le menu fait partir de l`'abom=nement
+        //    // Le propritaire doit voir tous les menus, le menu ne fesant pas partir de sont plan seron bloquer
+        //    // Propriétaire : tous les menus
+        //    menus = await _dbContext.Menus
+        //        .Where(m => !string.IsNullOrEmpty(m.Name))
+        //        .OrderBy(m => m.Order)
+        //        .Select(m => new MenuUserDTO
+        //        {
+        //            Id = m.Id.Value,
+        //            Name = m.Name,
+        //            UrlFront = m.UrlFront,
+        //            Icon = m.Icon
+        //        })
+        //        .ToListAsync();
+        //}
+        //else
+        //{
+        //    var userboutique = await _dbContext.UsersBoutiques
+        //    .FirstOrDefaultAsync(ub => ub.BoutiqueId == BoutiqueId.Of(boutiqueId) && ub.UserId == userId);
+
+        //    // Utilisateur classique : menus du profil
+        //    menus = await _dbContext.Profiles
+        //    .Where(p => p.Id == userboutique.ProfileId && p.IsActive)
+        //    .SelectMany(p => p.ProfileMenus.Where(pm => pm.IsActive && pm.Menu != null).OrderBy(m => m.Menu.Order))
+        //    .Select(pm => new MenuUserDTO
+        //    {
+        //        Id = pm.Menu.Id.Value,
+        //        Name = pm.Menu.Name,
+        //        UrlFront = pm.Menu.UrlFront,
+        //        Icon = pm.Menu.Icon
+        //    })
+        //    .ToListAsync();
+        //}
+
+        var result = await menuServiceApi.GetAllActifMenuAsync("depensio");
+        if (!result.Success)
         {
-            //TODO: (A revoir, reccuperer les menus du plan) deja correct
-            // juste mettre un filtre sur chaque API pour s'assurer que le menu fait partir de l`'abom=nement
-            // Le propritaire doit voir tous les menus, le menu ne fesant pas partir de sont plan seron bloquer
-            // Propriétaire : tous les menus
-            menus = await _dbContext.Menus
-                .Where(m => !string.IsNullOrEmpty(m.Name))
-                .OrderBy(m => m.Order)
-                .Select(m => new MenuUserDTO
-                {
-                    Id = m.Id.Value,
-                    Name = m.Name,
-                    UrlFront = m.UrlFront,
-                    Icon = m.Icon
-                })
-                .ToListAsync();
+            throw new BadRequestException(result.Message);
         }
-        else
-        {
-            var userboutique = await _dbContext.UsersBoutiques
-            .FirstOrDefaultAsync(ub => ub.BoutiqueId == BoutiqueId.Of(boutiqueId) && ub.UserId == userId);
 
-            // Utilisateur classique : menus du profil
-            menus = await _dbContext.Profiles
-            .Where(p => p.Id == userboutique.ProfileId && p.IsActive)
-            .SelectMany(p => p.ProfileMenus.Where(pm => pm.IsActive && pm.Menu != null).OrderBy(m => m.Menu.Order))
-            .Select(pm => new MenuUserDTO
-            {
-                Id = pm.Menu.Id.Value,
-                Name = pm.Menu.Name,
-                UrlFront = pm.Menu.UrlFront,
-                Icon = pm.Menu.Icon
-            })
-            .ToListAsync();
-        }
+        var menus = result.Data.Menus;
 
         return menus;
     }
