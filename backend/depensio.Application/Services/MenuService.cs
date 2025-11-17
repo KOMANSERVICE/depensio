6dbee40125a14d6336a626f7ff5d19c775e5374f
laserda <komanatsefabriceigor@gmail.com>
2025-11-16 22:30:37 -0500

    reccuperation des menus selon service menu

--- backend/depensio.Application/Services/MenuService.cs
+++ backend/depensio.Application/Services/MenuService.cs
@@ -14,51 +14,31 @@ public class MenuService(
 
 {
 
+    public async Task<List<MenuDTO>> GetMenuByBoutiqueAsync()
+    {
+        var result = await menuServiceApi.GetAllActifMenuAsync("depensio");
+        if (!result.Success)
+        {
+            throw new BadRequestException(result.Message);
+        }
+
+        var menus = result.Data.Menus.Select(m => new MenuDTO(
+                m.Reference,
+                m.Name
+            )).ToList();
+
+        return menus;
+    }
+
     public async Task<List<MenuUserDTO>> GetMenuByUserBoutiqueAsync(string userId, Guid boutiqueId)
     {
-        //var isOwner = await _dbContext.Boutiques
-        //    .AnyAsync(b => b.Id == BoutiqueId.Of(boutiqueId)
-        //    && b.OwnerId == userId
-        //    && b.UsersBoutiques.Any(ub => ub.UserId == userId));
+        var isOwner = await _dbContext.Boutiques
+            .AnyAsync(b => b.Id == BoutiqueId.Of(boutiqueId)
+            && b.OwnerId == userId
+            && b.UsersBoutiques.Any(ub => ub.UserId == userId));
 
-        //List<MenuUserDTO> menus;
+        List<MenuUserDTO> menus;
 
-        //if (isOwner)
-        //{
-        //    //TODO: (A revoir, reccuperer les menus du plan) deja correct
-        //    // juste mettre un filtre sur chaque API pour s'assurer que le menu fait partir de l`'abom=nement
-        //    // Le propritaire doit voir tous les menus, le menu ne fesant pas partir de sont plan seron bloquer
-        //    // Propriétaire : tous les menus
-        //    menus = await _dbContext.Menus
-        //        .Where(m => !string.IsNullOrEmpty(m.Name))
-        //        .OrderBy(m => m.Order)
-        //        .Select(m => new MenuUserDTO
-        //        {
-        //            Id = m.Id.Value,
-        //            Name = m.Name,
-        //            UrlFront = m.UrlFront,
-        //            Icon = m.Icon
-        //        })
-        //        .ToListAsync();
-        //}
-        //else
-        //{
-        //    var userboutique = await _dbContext.UsersBoutiques
-        //    .FirstOrDefaultAsync(ub => ub.BoutiqueId == BoutiqueId.Of(boutiqueId) && ub.UserId == userId);
-
-        //    // Utilisateur classique : menus du profil
-        //    menus = await _dbContext.Profiles
-        //    .Where(p => p.Id == userboutique.ProfileId && p.IsActive)
-        //    .SelectMany(p => p.ProfileMenus.Where(pm => pm.IsActive && pm.Menu != null).OrderBy(m => m.Menu.Order))
-        //    .Select(pm => new MenuUserDTO
-        //    {
-        //        Id = pm.Menu.Id.Value,
-        //        Name = pm.Menu.Name,
-        //        UrlFront = pm.Menu.UrlFront,
-        //        Icon = pm.Menu.Icon
-        //    })
-        //    .ToListAsync();
-        //}
 
         var result = await menuServiceApi.GetAllActifMenuAsync("depensio");
         if (!result.Success)
@@ -66,7 +46,68 @@ public class MenuService(
             throw new BadRequestException(result.Message);
         }
 
-        var menus = result.Data.Menus;
+        var resultMenus = result.Data.Menus;
+
+        if (isOwner)
+        {
+            //TODO: (A revoir, reccuperer les menus du plan) deja correct
+            // juste mettre un filtre sur chaque API pour s'assurer que le menu fait partir de l`'abom=nement
+            // Le propritaire doit voir tous les menus, le menu ne fesant pas partir de sont plan seron bloquer
+            // Propriétaire : tous les menus
+            //menus = await _dbContext.Menus
+            //    .Where(m => !string.IsNullOrEmpty(m.Name))
+            //    .OrderBy(m => m.Order)
+            //    .Select(m => new MenuUserDTO
+            //    {
+            //        Id = m.Id.Value,
+            //        Name = m.Name,
+            //        UrlFront = m.UrlFront,
+            //        Icon = m.Icon
+            //    })
+            //    .ToListAsync();
+
+            menus = resultMenus;
+        }
+        else
+        {
+            var userboutique = await _dbContext.UsersBoutiques
+            .FirstOrDefaultAsync(ub => ub.BoutiqueId == BoutiqueId.Of(boutiqueId) && ub.UserId == userId);
+
+            // Liste des ID menus autorisés au profil
+            var profileMenuIds = await _dbContext.ProfileMenus
+                .Where(pm => pm.ProfileId == userboutique.ProfileId && pm.IsActive)
+                .Select(pm => pm.ReferenceMenu)
+                .ToListAsync();
+
+            menus = resultMenus
+            .Where(m => profileMenuIds.Contains(m.Reference))
+            .Select(m => new MenuUserDTO
+            {
+                Id = m.Id,
+                Name = m.Name,
+                UrlFront = m.UrlFront,
+                Icon = m.Icon
+            })
+            .ToList();
+
+            // Utilisateur classique : menus du profil
+            if(menus.Count == 0)
+            {
+                menus = await _dbContext.Profiles
+                .Where(p => p.Id == userboutique.ProfileId && p.IsActive)
+                .SelectMany(p => p.ProfileMenus.Where(pm => pm.IsActive && pm.Menu != null).OrderBy(m => m.Menu.Order))
+                .Select(pm => new MenuUserDTO
+                {
+                    Id = pm.Menu.Id.Value,
+                    Name = pm.Menu.Name,
+                    UrlFront = pm.Menu.UrlFront,
+                    Icon = pm.Menu.Icon
+                })
+                .ToListAsync();
+            }
+        }
+
+
 
         return menus;
     }
@@ -80,37 +121,73 @@ public class MenuService(
 
         MenuUserDTO menu;
 
+        var result = await menuServiceApi.GetAllActifMenuAsync("depensio");
+        if (!result.Success)
+        {
+            throw new BadRequestException(result.Message);
+        }
+
+        var resultMenus = result.Data.Menus;
+
+
         if (isOwner)
         {
             //TODO: A revoir, reccuperer les menus du plan
             // Propriétaire : tous les menus
-            menu = await _dbContext.Menus
+            //menu = await _dbContext.Menus
+            //    .Where(m => !string.IsNullOrEmpty(m.Name) && currentPath.StartsWith(m.UrlFront, StringComparison.OrdinalIgnoreCase))
+            //    .OrderBy(m => m.Order)
+            //    .Select(m => new MenuUserDTO
+            //    {
+            //        Id = m.Id.Value,
+            //        Name = m.Name,
+            //        UrlFront = m.UrlFront,
+            //        Icon = m.Icon
+            //    })
+            //    .FirstOrDefaultAsync() ?? new MenuUserDTO();
+
+            menu = resultMenus
                 .Where(m => !string.IsNullOrEmpty(m.Name) && currentPath.StartsWith(m.UrlFront, StringComparison.OrdinalIgnoreCase))
-                .OrderBy(m => m.Order)
-                .Select(m => new MenuUserDTO{
-                    Id=m.Id.Value,
-                    Name=m.Name,
-                    UrlFront = m.UrlFront,
-                    Icon = m.Icon
-                })
-                .FirstOrDefaultAsync() ?? new MenuUserDTO();
+                .FirstOrDefault() ?? new MenuUserDTO();
         }
         else
         {
             var userboutique = await _dbContext.UsersBoutiques
             .FirstOrDefaultAsync(ub => ub.BoutiqueId == BoutiqueId.Of(boutiqueId) && ub.UserId == userId);
 
-            // Utilisateur classique : menus du profil
-            menu = await _dbContext.Profiles
-            .Where(p => p.Id == userboutique.ProfileId && p.IsActive)
-            .SelectMany(p => p.ProfileMenus.Where(pm => pm.IsActive && pm.Menu != null && !string.IsNullOrEmpty(pm.Menu.Name) && currentPath.StartsWith(pm.Menu.UrlFront, StringComparison.OrdinalIgnoreCase)).OrderBy(m => m.Menu.Order))
-            .Select(pm => new MenuUserDTO
+            // Liste des ID menus autorisés au profil
+            var profileMenuIds = await _dbContext.ProfileMenus
+                .Where(pm => pm.ProfileId == userboutique.ProfileId && pm.IsActive)
+                .Select(pm => pm.ReferenceMenu)
+                .ToListAsync();
+
+            menu = resultMenus
+            .Where(m => profileMenuIds.Contains(m.Reference) && currentPath.StartsWith(m.UrlFront, StringComparison.OrdinalIgnoreCase))
+            .Select(m => new MenuUserDTO
             {
-                Id = pm.Menu.Id.Value,
-                Name = pm.Menu.Name,
-                UrlFront = pm.Menu.UrlFront,
-                Icon = pm.Menu.Icon
-            }).FirstOrDefaultAsync() ?? new MenuUserDTO();
+                Id = m.Id,
+                Name = m.Name,
+                UrlFront = m.UrlFront,
+                Icon = m.Icon
+            })
+            .FirstOrDefault();
+
+            // Utilisateur classique : menus du profil
+            if (menu == null)
+            {
+                // Utilisateur classique : menus du profil
+                menu = await _dbContext.Profiles
+                .Where(p => p.Id == userboutique.ProfileId && p.IsActive)
+                .SelectMany(p => p.ProfileMenus.Where(pm => pm.IsActive && pm.Menu != null && !string.IsNullOrEmpty(pm.Menu.Name) && currentPath.StartsWith(pm.Menu.UrlFront, StringComparison.OrdinalIgnoreCase)).OrderBy(m => m.Menu.Order))
+                .Select(pm => new MenuUserDTO
+                {
+                    Id = pm.Menu.Id.Value,
+                    Name = pm.Menu.Name,
+                    UrlFront = pm.Menu.UrlFront,
+                    Icon = pm.Menu.Icon
+                }).FirstOrDefaultAsync() ?? new MenuUserDTO();
+
+            }
         }
 
         return menu;
---
+++ backend/depensio.Infrastructure/Data/Migrations/20251117031929_AddReferenceProfileMenu.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace depensio.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReferenceProfileMenu : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ReferenceMenu",
+                table: "ProfileMenus",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReferenceMenu",
+                table: "ProfileMenus");
+        }
+    }
+}
